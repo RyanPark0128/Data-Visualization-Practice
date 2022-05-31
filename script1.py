d04fe25b4b2e40547f17b3952084687cccbffe75
RyanPark0128 <vazywon@hotmail.com>
2022-05-31 15:43:31 -0700

    explore Flask

--- script1.py
+++ script1.py
@@ -1,16 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return "Website content goes here!"
+    return render_template("home.html")
 
 
-@app.route('/about')
-def about():
-    return "about"
+@app.route('/about/')
+@app.route('/about/<topic>')
+def about(topic=None):
+    return render_template("about.html", topic=topic)
 
 
 if __name__ == "__main__":
