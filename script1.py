e13b857ee5940d93bbc27986f7833a5139e66ad1
RyanPark0128 <vazywon@hotmail.com>
2022-06-04 18:12:08 -0700

    add routes all together

--- script1.py
+++ script1.py
@@ -1,26 +1,3 @@
-# from flask import Flask, render_template
-
-# app = Flask(__name__)
-
-
-# @app.route('/')
-# def home():
-#     return render_template("home.html")
-
-
-# @app.route('/about/')
-# @app.route('/about/<topic>')
-# def about(topic=None):
-#     return render_template("about.html", topic=topic)
-
-
-# @app.route("/me")
-# def me_api():
-#     return {
-#         "username": "ryan",
-#         "theme": "black",
-#     }
-
 from flask import Flask, flash, redirect, render_template, \
     request, url_for
 
@@ -44,6 +21,25 @@ def login():
             flash('You were successfully logged in')
             return redirect(url_for('index'))
     return render_template('login.html', error=error)
+
+
+@app.route('/home')
+def home():
+    return render_template("home.html")
+
+
+@app.route('/about/')
+@app.route('/about/<topic>')
+def about(topic=None):
+    return render_template("about.html", topic=topic)
+
+
+@app.route("/me")
+def me_api():
+    return {
+        "username": "ryan",
+        "theme": "black",
+    }
 
 
 if __name__ == "__main__":
